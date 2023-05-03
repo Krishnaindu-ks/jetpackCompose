762302201f0cc195dcc31082e8e59ade75f511c2
Krishnaindu-ks <krishnaindu1902@gmail.com>
2023-05-03 16:48:06 +0530

    Jetpack Compose Basics

---
+++ app/src/main/java/com/ks/jetpackcomposebasic/MainActivity.kt
@@ -0,0 +1,147 @@
+package com.ks.jetpackcomposebasic
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ks.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeBasicTheme {
+                // A surface container using the 'background' color from the theme
+               MyApp(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+@Composable
+ fun MyApp(modifier: Modifier = Modifier){
+    var shouldShowOnboarding by rememberSaveable{ mutableStateOf(true) }
+    Surface(modifier) {
+        if (shouldShowOnboarding){
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false})
+        } else {
+            Greetings()
+        }
+
+    }
+
+ }
+@Composable
+fun OnboardingScreen( onContinueClicked: () -> Unit, modifier: Modifier=Modifier) {
+    Column(modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center ,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Text("Welcome To Jetpack Compose Basics")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        )
+        {
+            Text("Continue")
+        }
+    }
+}
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names:List<String> = List(1000) {"$it" }
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+
+//    Surface (
+//        modifier = modifier , color = MaterialTheme.colorScheme.background)
+//    {
+//        Greeting("Android")
+//    }
+
+}
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    JetpackComposeBasicTheme{
+        OnboardingScreen(onContinueClicked = {})
+    }
+
+}
+@Composable
+private fun Greeting(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+   Surface(color = MaterialTheme.colorScheme.primary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+       Row(modifier = Modifier.padding(24.dp)){
+           Column(modifier = Modifier
+               .weight(1f)
+               .padding(bottom = extraPadding.coerceAtLeast(0.dp))){
+               Text(text = "Hello,")
+               Text(text = name)
+           }
+           ElevatedButton(
+               onClick = { expanded = !expanded }
+           ) {
+               Text(if (expanded) "Show less" else "Show more")
+           }
+       }
+      }
+   }
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeBasicTheme{
+        Greetings()
+    }
+}
+@Preview
+@Composable
+fun MyAppPreview() {
+    JetpackComposeBasicTheme{
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+//@Preview(showBackground = true , widthDp = 320)
+//@Composable
+//private fun GreetingsPreview() {
+//    JetpackComposeBasicTheme{
+//        Greetings()
+//    }
+//}
